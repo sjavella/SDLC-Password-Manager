4a3435edcef72acd7d06fc41ad93f4154056c6f9
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2021-05-02 16:36:34 -1000

    landing info added

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -12,8 +12,18 @@ class Landing extends React.Component {
         </Grid.Column>
 
         <Grid.Column width={8}>
-          <h1>Password Manager</h1>
-          <p>Info & security disclaimer</p>
+          <Header> SDLC Password Manager </Header>
+          <p>Warning: This app was developed as an exercise in the Secure Development LifeCycle, and is not inteded to be used as a secure apppcation.</p>
+          <Header size='small'>Security Features:</Header>
+          <p> User accounts - passwords encrypted with bcrypt js. Bcrypt was choosen for securing the user account feature because it
+               it is a one way hashing algorithm, meaning it doesnt have the ability to decrypt hash's, only to compare hashed
+               codes, making it an optimal choice for a program with a small attack surface. </p>
+          <p> Admin account - implemented in inital development, removed so as to reduce attack surface area and cut down on redundancy. </p>
+          <p> Sensitive Databases - All sensitive information is hashed before it gets stored to the database, allowing for the secure addition of data.</p>
+          <p> Encryption - All added passwords encrypted with cryptr. Cryptr was choosen because it allows for 2 way hashing, meaning all hashed
+               passwords can be decrypted for the user to see. It is less efficient than bcrypt because its not asynchronous, and therefore takes longer to hash.</p>
+          <p> Decryption - Automatically re-encrypts shown password after 10 seconds </p>
+          <p> Secure Edit/Delete Functions - All data is encrypted before being re-added or removed to/from a collection. </p>
         </Grid.Column>
 
       </Grid>
